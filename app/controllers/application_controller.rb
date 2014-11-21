60aaf0773cf815bbb238313bea733143f863204f
Aaron Groch <ar.groch@gmail.com>
2014-11-21 15:22:14 -0500

    working on styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
 
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :name, :username, :bio, :location, :birthday, :following) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation, :name, :username, :bio, :location, :birthday, :following, :avatar, :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at) }
 
     devise_parameter_sanitizer.for(:account_update) { |u| 
-      u.permit(:email, :password, :password_confirmation, :current_password, :name, :username, :bio, :location, :birthday, :following) 
+      u.permit(:email, :password, :password_confirmation, :current_password, :name, :username, :bio, :location, :birthday, :following, :avatar, :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at) 
     }
 
 
